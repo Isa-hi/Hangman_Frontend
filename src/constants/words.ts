ca85c737a56eea45f1089c3376e4b14611ec3899
Jared Muñoz <amunozbaez669@gmail.com>
2024-10-25 17:02:03 -0600

    feat: add some words store in case there are no words on local storage

---
+++ src/constants/words.ts
@@ -0,0 +1,10 @@
+export const WORDS = [
+    "elephant", "giraffe", "penguin", "kangaroo", "dolphin",
+    "turquoise", "lasagna", "croissant", "pancake", "burrito",
+    "Argentina", "Singapore", "Australia", "Portugal", "Denmark",
+    "tornado", "blizzard", "architect", "scientist", "musician",
+    "Amsterdam", "Barcelona", "Istanbul", "Kyoto", "Vancouver",
+    "asteroid", "galaxy", "meteorite", "nebula", "supernova",
+    "unicorn", "dragon", "phoenix", "werewolf", "griffin",
+    "algorithm", "processor", "database", "encryption", "bandwidth"
+];
